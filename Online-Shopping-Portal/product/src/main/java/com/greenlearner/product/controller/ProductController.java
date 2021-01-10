dc528142ba0f427a7ea01e392d7a0826219b8bf5
Green Learner <greenlearner01@users.noreply.github.com>
2021-01-10 12:44:32 +0530

    Adding data validation https://youtu.be/_RiyhX3wyvc
    complete playlist about rest api validation - https://youtube.com/playlist?list=PLq3uEqRnr_2EjFzGspecJgPvWRU8jrT31

--- Online-Shopping-Portal/product/src/main/java/com/greenlearner/product/controller/ProductController.java
+++ Online-Shopping-Portal/product/src/main/java/com/greenlearner/product/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -26,7 +27,7 @@ public class ProductController {
     }
 
     @PostMapping("/addProduct")
-    ResponseEntity<Product> addProduct(@RequestBody Product product){
+    ResponseEntity<Product> addProduct(@RequestBody @Valid Product product){
 
         String status = productService.addProduct(product);
 
--- Online-Shopping-Portal/product/src/main/java/com/greenlearner/product/dto/Product.java
+++ Online-Shopping-Portal/product/src/main/java/com/greenlearner/product/dto/Product.java
@@ -4,6 +4,9 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
@@ -19,10 +22,19 @@ public class Product {
 
     @Id
     private Integer id;
+
+    @NotNull(message = "Product name should not be null")
     private String name;
+
+    @NotNull(message = "Category should not be null")
     private Category category;
+
+    @Min(0)
+
     private double price;
+
     private String currency;
+    @Max(100)
     private double discount;
     private String discountDescription;
     private List<String> imageURLs;
