a584411106c2eff8aec340adb60895fed4c0eb84
Green Learner <whytechnology01@gmail.com>
2022-01-31 10:43:15 +0530

    #22.5.3 How Spring Security Works Internally? | User Service
    https://youtu.be/G-4n_BQH48o

--- Online-Shopping-Portal/userservice/src/main/java/com/gl/userservice/UserserviceApplication.java
+++ Online-Shopping-Portal/userservice/src/main/java/com/gl/userservice/UserserviceApplication.java
@@ -2,8 +2,10 @@ package com.gl.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
